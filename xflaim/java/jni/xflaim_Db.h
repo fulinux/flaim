6396b845b1c3aa521498723f1443e9e1f264064f
dsandersoremutah <dsandersoremutah@0109f412-320b-0410-ab79-c3e0c5ffbbe6>
2006-08-04 21:56:10 +0000

    Added a bunch of new functions for the F_Db class.
    git-svn-id: http://svn.code.sf.net/p/flaim/code/trunk@737 0109f412-320b-0410-ab79-c3e0c5ffbbe6

--- xflaim/java/jni/xflaim_Db.h
+++ xflaim/java/jni/xflaim_Db.h
@@ -9,6 +9,110 @@ extern "C" {
 #endif
 /*
  * Class:     xflaim_Db
+ * Method:    _dbLock
+ * Signature: (JIII)V
+ */
+JNIEXPORT void JNICALL Java_xflaim_Db__1dbLock
+  (JNIEnv *, jobject, jlong, jint, jint, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _dbUnlock
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_xflaim_Db__1dbUnlock
+  (JNIEnv *, jobject, jlong);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getLockType
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1getLockType
+  (JNIEnv *, jobject, jlong);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getLockImplicit
+ * Signature: (J)Z
+ */
+JNIEXPORT jboolean JNICALL Java_xflaim_Db__1getLockImplicit
+  (JNIEnv *, jobject, jlong);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getLockThreadId
+ * Signature: (JI)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1getLockThreadId
+  (JNIEnv *, jobject, jlong, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getLockNumExclQueued
+ * Signature: (JI)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1getLockNumExclQueued
+  (JNIEnv *, jobject, jlong, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getLockNumSharedQueued
+ * Signature: (JI)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1getLockNumSharedQueued
+  (JNIEnv *, jobject, jlong, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getLockPriorityCount
+ * Signature: (JI)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1getLockPriorityCount
+  (JNIEnv *, jobject, jlong, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _indexSuspend
+ * Signature: (JI)V
+ */
+JNIEXPORT void JNICALL Java_xflaim_Db__1indexSuspend
+  (JNIEnv *, jobject, jlong, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _indexResume
+ * Signature: (JI)V
+ */
+JNIEXPORT void JNICALL Java_xflaim_Db__1indexResume
+  (JNIEnv *, jobject, jlong, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _reduceSize
+ * Signature: (JI)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1reduceSize
+  (JNIEnv *, jobject, jlong, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _documentDone
+ * Signature: (JIJ)V
+ */
+JNIEXPORT void JNICALL Java_xflaim_Db__1documentDone__JIJ
+  (JNIEnv *, jobject, jlong, jint, jlong);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _documentDone
+ * Signature: (JJ)V
+ */
+JNIEXPORT void JNICALL Java_xflaim_Db__1documentDone__JJ
+  (JNIEnv *, jobject, jlong, jlong);
+
+/*
+ * Class:     xflaim_Db
  * Method:    _release
  * Signature: (J)V
  */
@@ -20,8 +124,16 @@ JNIEXPORT void JNICALL Java_xflaim_Db__1release
  * Method:    _transBegin
  * Signature: (JIII)V
  */
-JNIEXPORT void JNICALL Java_xflaim_Db__1transBegin
+JNIEXPORT void JNICALL Java_xflaim_Db__1transBegin__JIII
   (JNIEnv *, jobject, jlong, jint, jint, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _transBegin
+ * Signature: (JJ)V
+ */
+JNIEXPORT void JNICALL Java_xflaim_Db__1transBegin__JJ
+  (JNIEnv *, jobject, jlong, jlong);
 
 /*
  * Class:     xflaim_Db
@@ -41,6 +153,22 @@ JNIEXPORT void JNICALL Java_xflaim_Db__1transAbort
 
 /*
  * Class:     xflaim_Db
+ * Method:    _getTransType
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1getTransType
+  (JNIEnv *, jobject, jlong);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _doCheckpoint
+ * Signature: (JI)V
+ */
+JNIEXPORT void JNICALL Java_xflaim_Db__1doCheckpoint
+  (JNIEnv *, jobject, jlong, jint);
+
+/*
+ * Class:     xflaim_Db
  * Method:    _import
  * Signature: (JLxflaim/PosIStream;I)V
  */
@@ -50,10 +178,26 @@ JNIEXPORT void JNICALL Java_xflaim_Db__1import
 /*
  * Class:     xflaim_Db
  * Method:    _getFirstDocument
- * Signature: (JILxflaim/DOMNode;)J
+ * Signature: (JIJ)J
  */
 JNIEXPORT jlong JNICALL Java_xflaim_Db__1getFirstDocument
-  (JNIEnv *, jobject, jlong, jint, jobject);
+  (JNIEnv *, jobject, jlong, jint, jlong);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getLastDocument
+ * Signature: (JIJ)J
+ */
+JNIEXPORT jlong JNICALL Java_xflaim_Db__1getLastDocument
+  (JNIEnv *, jobject, jlong, jint, jlong);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getDocument
+ * Signature: (JIIJJ)J
+ */
+JNIEXPORT jlong JNICALL Java_xflaim_Db__1getDocument
+  (JNIEnv *, jobject, jlong, jint, jint, jlong, jlong);
 
 /*
  * Class:     xflaim_Db
@@ -62,6 +206,14 @@ JNIEXPORT jlong JNICALL Java_xflaim_Db__1getFirstDocument
  */
 JNIEXPORT jlong JNICALL Java_xflaim_Db__1getNode
   (JNIEnv *, jobject, jlong, jint, jlong, jlong);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getAttribute
+ * Signature: (JIJIJ)J
+ */
+JNIEXPORT jlong JNICALL Java_xflaim_Db__1getAttribute
+  (JNIEnv *, jobject, jlong, jint, jlong, jint, jlong);
 
 /*
  * Class:     xflaim_Db
@@ -86,6 +238,126 @@ JNIEXPORT jlong JNICALL Java_xflaim_Db__1createRootElement
  */
 JNIEXPORT jint JNICALL Java_xflaim_Db__1createElementDef
   (JNIEnv *, jobject, jlong, jstring, jstring, jint, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getElementNameId
+ * Signature: (JLjava/lang/String;Ljava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1getElementNameId
+  (JNIEnv *, jobject, jlong, jstring, jstring);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _createUniqueElmDef
+ * Signature: (JLjava/lang/String;Ljava/lang/String;I)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1createUniqueElmDef
+  (JNIEnv *, jobject, jlong, jstring, jstring, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _createAttributeDef
+ * Signature: (JLjava/lang/String;Ljava/lang/String;II)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1createAttributeDef
+  (JNIEnv *, jobject, jlong, jstring, jstring, jint, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getAttributeNameId
+ * Signature: (JLjava/lang/String;Ljava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1getAttributeNameId
+  (JNIEnv *, jobject, jlong, jstring, jstring);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _createPrefixDef
+ * Signature: (JLjava/lang/String;I)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1createPrefixDef
+  (JNIEnv *, jobject, jlong, jstring, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getPrefixId
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1getPrefixId
+  (JNIEnv *, jobject, jlong, jstring);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _createEncDef
+ * Signature: (JLjava/lang/String;Ljava/lang/String;II)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1createEncDef
+  (JNIEnv *, jobject, jlong, jstring, jstring, jint, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getEncDefId
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1getEncDefId
+  (JNIEnv *, jobject, jlong, jstring);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _createCollectionDef
+ * Signature: (JLjava/lang/String;II)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1createCollectionDef
+  (JNIEnv *, jobject, jlong, jstring, jint, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getCollectionNumber
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1getCollectionNumber
+  (JNIEnv *, jobject, jlong, jstring);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getIndexNumber
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_xflaim_Db__1getIndexNumber
+  (JNIEnv *, jobject, jlong, jstring);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getDictionaryDef
+ * Signature: (JIIJ)J
+ */
+JNIEXPORT jlong JNICALL Java_xflaim_Db__1getDictionaryDef
+  (JNIEnv *, jobject, jlong, jint, jint, jlong);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getDictionaryName
+ * Signature: (JII)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_xflaim_Db__1getDictionaryName
+  (JNIEnv *, jobject, jlong, jint, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getElementNamespace
+ * Signature: (JI)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_xflaim_Db__1getElementNamespace
+  (JNIEnv *, jobject, jlong, jint);
+
+/*
+ * Class:     xflaim_Db
+ * Method:    _getAttributeNamespace
+ * Signature: (JI)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_xflaim_Db__1getAttributeNamespace
+  (JNIEnv *, jobject, jlong, jint);
 
 /*
  * Class:     xflaim_Db
