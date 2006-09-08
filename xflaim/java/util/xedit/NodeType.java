f55b42845db0664736120d5f8bad573cdbec9215
dsandersoremutah <dsandersoremutah@0109f412-320b-0410-ab79-c3e0c5ffbbe6>
2006-09-08 15:48:38 +0000

    Moved xedit files.
    git-svn-id: http://svn.code.sf.net/p/flaim/code/trunk@836 0109f412-320b-0410-ab79-c3e0c5ffbbe6

---
+++ xflaim/java/util/xedit/NodeType.java
@@ -0,0 +1,50 @@
+//------------------------------------------------------------------------------
+// Desc:	Node Type
+//
+// Tabs:	3
+//
+//		Copyright (c) 2003,2005-2006 Novell, Inc. All Rights Reserved.
+//
+//		This program is free software; you can redistribute it and/or
+//		modify it under the terms of version 2 of the GNU General Public
+//		License as published by the Free Software Foundation.
+//
+//		This program is distributed in the hope that it will be useful,
+//		but WITHOUT ANY WARRANTY; without even the implied warranty of
+//		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//		GNU General Public License for more details.
+//
+//		You should have received a copy of the GNU General Public License
+//		along with this program; if not, contact Novell, Inc.
+//
+//		To contact Novell about this file by physical or electronic mail,
+//		you may find current contact information at www.novell.com
+//
+// $Id: NodeType.java 3120 2006-01-19 13:41:12 -0700 (Thu, 19 Jan 2006) dsanders $
+//------------------------------------------------------------------------------
+package xedit;
+
+import java.awt.Component;
+
+/**
+ * To change the template for this generated type comment go to
+ * Window->Preferences->Java->Code Generation->Code and Comments
+ */
+public class NodeType extends Component
+{
+	String		m_sType;
+	int			m_iType;
+	
+	public NodeType(
+		String		sType,
+		int			iType)
+	{
+		m_sType = sType;
+		m_iType = iType;
+	}
+	
+	public String toString()
+	{
+		return m_sType;
+	}
+}
