f892b84f0a543aa3fdb8ebb8a72d78e645b7e29b
jcalcote <jcalcote@0109f412-320b-0410-ab79-c3e0c5ffbbe6>
2008-07-02 00:25:17 +0000

    Added batch files to the win32 project directories so the projects can be built from the command line. Made minor tweaks to a few projects.
    git-svn-id: http://svn.code.sf.net/p/flaim/code/trunk@1072 0109f412-320b-0410-ab79-c3e0c5ffbbe6

---
+++ ftk/win32/ftk/buildall.cmd
@@ -0,0 +1,24 @@
+@ECHO OFF
+
+setlocal
+
+set solution=ftk.sln
+set operation=Build
+
+set build="Release"
+if     "%1" == "debug"   (set build="Debug" && goto do_build)
+if     "%1" == "release" (set build="Release" && goto do_build)
+if NOT "%1" == ""         goto help
+
+:do_build
+echo %operation%ing %solution% %build% build...
+devenv flaim-projects.sln /%operation% %build%
+
+goto done
+
+:help
+echo Usage: %0 [release^|debug]
+echo %operation%s the "release" build by default.
+
+:done
+endlocal
